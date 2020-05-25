8cdd0253dd8effa20a57481c547f5d0f9744f145
aungwanakyimal <59996144+aungwanakyimal@users.noreply.github.com>
2020-05-25 02:21:00 -0700

    Update btn.js

--- btn.js
+++ btn.js
@@ -1,7 +1,4 @@
-$(window).on('load', function () {
-    $('#m3u8-placeholder')[0].value = localStorage.getItem('m3u8-link') || '';
-    $('#play-btn').on('click', function () {
-        localStorage.setItem('m3u8-link', $('#m3u8-placeholder')[0].value);
-        window.location.href = './player' + '#' + $('#m3u8-placeholder')[0].value;
-    });
-});
+.btn{color:#ffffff;background-color:#2989cc;border-color:#000000;}
+.btn{font-size: 20px;border-radius: 40px 40px 40px 0px;font-weight: normal;font-weight: bold;font-weight: 900;}
+.btn{height: 50px;width: 150px;}
+.btn{-webkit-text-stroke: 0.3px black; text-shadow: -1px -1px 0 #000;}
